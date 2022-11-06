8a22daf3489f5bb75ba1b7aa5ce7b45ca72c5da3
vlad4k5 <armia1823@gmail.com>
2022-11-06 16:54:55 +0200

    add dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import Header from './components/Header/Header'
 import './assets/styles/global.scss'
 
 
-
 const App = () => {
   return (
     <AppContainer>
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,30 @@
-import {ReactComponent as moon} from '../../assets/img/moon.svg'
+import { useEffect, useState } from 'react'
+import {ReactComponent as MoonIcon} from '../../assets/img/moon.svg'
+import {ReactComponent as SunIcon} from '../../assets/img/sun.svg'
 import s from './Header.module.scss'
 
 
 const Header = () => {
+  const [theme, setTheme] = useState('light')
 
-  const ThemeIcon = moon
+  const toggleTheme = () => {
+    setTheme(theme === 'light' ? 'dark' : 'light')
+  }
+
+  useEffect(() => {
+    document.body.setAttribute('data-theme', theme)
+  }, [theme])
+
+  const ThemeIcon = theme === 'light' ?  MoonIcon : SunIcon
+
   return (
     <header className={s.header}>
       <div className={s.logo}>
         devfinder
       </div>
 
-      <div className={s.themeSwitcher}>
-        <span className={s.themeName}>Dark</span>
+      <div className={s.themeSwitcher} onClick={toggleTheme}>
+        <span className={s.themeName}>{theme === 'light' ? 'dark' : 'light'}</span>
         <ThemeIcon />
       </div>
     </header>
